a778351ced09ef2ddb58ba824a332645cceba180
chenenyu <chenenyux@gmail.com>
2023-08-23 10:40:59 +0800

    export two mixins

--- lib/lifecycle.dart
+++ lib/lifecycle.dart
@@ -1,6 +1,7 @@
 library lifecycle;
 
 export 'src/child_page_lifecycle_wrapper.dart';
+export 'src/child_page_subscribe_lifecycle_mixin.dart';
 export 'src/lifecycle_aware.dart';
 export 'src/lifecycle_mixin.dart';
 export 'src/lifecycle_observer.dart';
@@ -8,3 +9,4 @@ export 'src/lifecycle_wrapper.dart';
 export 'src/page_view_lifecycle_wrapper.dart';
 export 'src/scroll_view_item_lifecycle_wrapper.dart';
 export 'src/scroll_view_item_subscribe_lifecycle_mixin.dart';
+export 'src/widget_dispatch_lifecycle_mixin.dart';
